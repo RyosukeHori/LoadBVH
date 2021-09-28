18160b4eecd2853a14d0e2ca8611a89a50c7755d
RyosukeHori <hori.rysk@icloud.com>
2021-09-28 19:40:20 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import numpy as np
+import argparse
+import math
+from bvh import Bvh
+from skeleton import Skeleton
+import glob
+import argparse
+
+sys.path.append(os.getcwd())
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mocap-fr', type=int, default=50)
+parser.add_argument('--dt', type=float, default=1 / 50)
+args = parser.parse_args()
+
+
+def load_bvh_file(fname, skeleton):
+    with open(fname) as f:
+        mocap = Bvh(f.read())
+
+    # build bone_addr
+    bone_addr = dict()
+    start_ind = 0
+    for bone in skeleton.bones:
+        end_ind = start_ind + len(bone.channels)
+        bone_addr[bone.name] = (start_ind, end_ind)
+        start_ind = end_ind
+    dof_num = start_ind
+
+    poses = np.zeros((mocap.nframes, dof_num))
+    for i in range(mocap.nframes):
+        for bone in skeleton.bones:
+            trans = np.array(mocap.frame_joint_channels(i, bone.name, bone.channels))
+            start_ind, end_ind = bone_addr[bone.name]
+            poses[i, start_ind:end_ind] = trans
+
+    return poses, bone_addr
+
+
+def main():
+    skt_bvh = './test.bvh'
+    exclude_bones = {'Hand', 'Foot', 'End', 'Toe', 'Head'}
+    spec_channels = {'LeftForeArm': ['Xrotation', 'Yrotation'], 'RightForeArm': ['Xrotation', 'Yrotation'],
+                     'LeftLeg': ['Xrotation'], 'RightLeg': ['Xrotation'],
+                     'LeftFoot': ['Xrotation', 'Yrotation'], 'RightFoot': ['Xrotation', 'Yrotation']}
+    skeleton = Skeleton()
+    skeleton.load_from_bvh(skt_bvh, exclude_bones, spec_channels)
+
+    bvh_files = glob.glob(os.path.expanduser('./*.bvh'))
+    bvh_files.sort()
+    print(bvh_files)
+    for file in bvh_files:
+        print('extracting trajectory from %s' % file)
+        poses, bone_addr = load_bvh_file(file, skeleton)
+        error_num = 0
+        for frame, pose in enumerate(poses):
+            error = False
+            for bone, addr in bone_addr.items():
+                lb = skeleton.name2bone[bone].lb
+                ub = skeleton.name2bone[bone].ub
+                rot = pose[addr[0]:addr[1]]
+                error_rot = False
+                for idx, val in enumerate(rot):
+                    if val < lb[idx] or val > ub[idx]:
+                        error = error_rot = True
+                if error_rot:
+                    print(' frame:', frame, ' bone:', bone, ' rot:', rot.tolist(), ' lb:', lb, ' ub:', ub)
+            if error:
+                error_num += 1
+        print(' Error frame num:', error_num, ' Percentage:', error_num / poses.shape[0] * 100, '%')
+
+if __name__ == '__main__':
+    main()
---
+++ skeleton.py
@@ -0,0 +1,130 @@
+import math
+import re
+from bvh import Bvh
+import numpy as np
+
+
+class Bone:
+
+    def __init__(self):
+        # original bone info
+        self.id = None
+        self.name = None
+        self.orient = np.identity(3)
+        self.dof_index = []
+        self.channels = []  # bvh only
+        self.lb = []
+        self.ub = []
+        self.parent = None
+        self.child = []
+
+        # bvh specific
+        self.offset = np.zeros(3)
+
+        # inferred info
+        self.pos = np.zeros(3)
+        self.end = np.zeros(3)
+
+
+class Skeleton:
+
+    def __init__(self):
+        self.bones = []
+        self.name2bone = {}
+        self.mass_scale = 1.0
+        self.len_scale = 1.0
+        self.dof_name = ['z', 'x', 'y']
+        self.root = None
+
+    RoM = {            # [[x_lb, y_lb, z_lb],    [x_ub, y_ub, z_ub]]
+        'Hips':          [[-180, -180, -180],    [180, 180, 180]],
+        'LeftUpLeg':     [[-100, -40, -40],      [30, 60, 40]],
+        'LeftLeg':       [[-10, -10, -10],       [150, 10, 10]],
+        'LeftFoot':      [[-20, -10, -1],        [40, 30, 1]],
+        'RightUpLeg':    [[-100, -60, -40],      [30, 40, 40]],
+        'RightLeg':      [[-10, -10, -10],       [150, 10, 10]],
+        'RightFoot':     [[-20, -30, -1],        [40, 10, 1]],
+        'Spine':         [[-10, -10, -10],       [15, 10, 10]],
+        'Spine1':        [[-10, -10, -10],       [15, 10, 10]],
+        'Spine2':        [[-10, -10, -10],       [15, 10, 10]],
+        'Spine3':        [[-10, -10, -10],       [15, 10, 10]],
+        'Spine4':        [[-10, -10, -10],       [15, 10, 10]],
+        'Spine5':        [[-10, -10, -10],       [15, 10, 10]],
+        'Spine6':        [[-10, -10, -10],       [15, 10, 10]],
+        'LeftShoulder':  [[-20, -20, -20],       [20, 20, 20]],
+        'LeftArm':       [[-40, -120, -100],     [120, 30, 40]],
+        'LeftForeArm':   [[-80, -150, -1],       [80, 1, 1]],
+        'LeftHand':      [[-1, -80, -30],        [1, 60, 20]],
+        'RightShoulder': [[-20, -20, -20],       [20, 20, 20]],
+        'RightArm':      [[-40, -30, -40],       [120, 120, 100]],
+        'RightForeArm':  [[-80, -1, -1],         [80, 150, 1]],
+        'RightHand':     [[-1, -60, -20],        [1, 80, 30]],
+        'Neck':          [[-30, -30, -30],       [45, 30, 30]]
+    }
+
+    def load_from_bvh(self, fname, exclude_bones=None, spec_channels=None):
+        if exclude_bones is None:
+            exclude_bones = {}
+        if spec_channels is None:
+            spec_channels = dict()
+        with open(fname) as f:
+            mocap = Bvh(f.read())
+
+        joint_names = list(filter(lambda x: all([t not in x for t in exclude_bones]), mocap.get_joints_names()))
+        dof_ind = {'x': 1, 'y': 2, 'z': 0}
+        #self.len_scale = 0.0254
+        self.root = Bone()
+        self.root.id = 0
+        self.root.name = joint_names[0]
+        self.root.channels = mocap.joint_channels(self.root.name)[3:]
+        self.root.lb = [self.RoM[self.root.name][0][2], self.RoM[self.root.name][0][0], self.RoM[self.root.name][0][1]]
+        self.root.ub = [self.RoM[self.root.name][1][2], self.RoM[self.root.name][1][0], self.RoM[self.root.name][1][1]]
+        self.name2bone[self.root.name] = self.root
+        self.bones.append(self.root)
+        for i, joint in enumerate(joint_names[1:]):
+            bone = Bone()
+            bone.id = i + 1
+            bone.name = joint
+
+            # pos and rot
+            '''
+            if joint in spec_channels.keys():
+                spec_channel = mocap.joint_channels(joint)[:3]
+                spec_channel.extend(spec_channels[joint])
+                bone.channels = spec_channel
+            else:
+                bone.channels = mocap.joint_channels(joint)
+            '''
+            # rot
+            #bone.channels = spec_channels[joint] if joint in spec_channels.keys() else mocap.joint_channels(joint)[3:]
+            bone.channels = mocap.joint_channels(joint)[3:]
+
+            bone.dof_index = [dof_ind[x[0].lower()] for x in bone.channels]
+            bone.offset = np.array(mocap.joint_offset(joint)) * self.len_scale
+            bone.lb = [self.RoM[joint][0][2], self.RoM[joint][0][0], self.RoM[joint][0][1]]  # (z, x, y)
+            bone.ub = [self.RoM[joint][1][2], self.RoM[joint][1][0], self.RoM[joint][1][1]]
+            self.bones.append(bone)
+            self.name2bone[joint] = bone
+
+        for bone in self.bones[1:]:
+            parent_name = mocap.joint_parent(bone.name).name
+            if parent_name in self.name2bone.keys():
+                bone_p = self.name2bone[parent_name]
+                bone_p.child.append(bone)
+                bone.parent = bone_p
+
+        self.forward_bvh(self.root)
+        for bone in self.bones:
+            if len(bone.child) == 0:
+                bone.end = bone.pos + np.array(
+                    [float(x) for x in mocap.get_joint(bone.name).children[-1]['OFFSET']]) * self.len_scale
+            else:
+                bone.end = sum([bone_c.pos for bone_c in bone.child]) / len(bone.child)
+
+    def forward_bvh(self, bone):
+        if bone.parent:
+            bone.pos = bone.parent.pos + bone.offset
+        else:
+            bone.pos = bone.offset
+        for bone_c in bone.child:
+            self.forward_bvh(bone_c)
